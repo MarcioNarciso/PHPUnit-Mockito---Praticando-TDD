de9a954bd52f3abe793045fe615ff4a08ece970d
Márcio Narciso D. <marcionarciso@live.com>
2024-03-06 15:36:56 -0300

    refat: refatornando o código da CalculadoraDeSalario, separando as fórmulas em métodos privados.

--- src/app/Loja/RH/CalculadoraDeSalario.php
+++ src/app/Loja/RH/CalculadoraDeSalario.php
@@ -12,23 +12,30 @@ class CalculadoraDeSalario {
     public function calcularSalario(Funcionario $funcionario) : float
     {
         if ($funcionario->getCargo() == TabelaCargos::DESENVOLVEDOR) {
-            if ($funcionario->getSalario() > 3000) {
-                return $funcionario->getSalario() * 0.8;
-            }
-
-            return $funcionario->getSalario() * 0.9;
+            return $this->dezOuVintePorcentoDeDesconto($funcionario);
         }
         
-        if ($funcionario->getCargo() == TabelaCargos::DBA) {
-            if ($funcionario->getSalario() > 2500) {
-                return $funcionario->getSalario() * 0.75;
-            }
-
-            return $funcionario->getSalario() * 0.85;
+        if ($funcionario->getCargo() == TabelaCargos::DBA
+                || $funcionario->getCargo() == TabelaCargos::TESTADOR) {
+            return $this->quinzeOuVinteECincoPorcentoDeDesconto($funcionario);
         }
         
         throw new \Exception("Tipo de funcionário inválido.");
     }
-    
-    
+
+    private function dezOuVintePorcentoDeDesconto($funcionario) {
+        if ($funcionario->getSalario() > 3000) {
+            return $funcionario->getSalario() * 0.8;
+        }
+
+        return $funcionario->getSalario() * 0.9;
+    }
+
+    private function quinzeOuVinteECincoPorcentoDeDesconto($funcionario) {
+        if ($funcionario->getSalario() > 2500) {
+            return $funcionario->getSalario() * 0.75;
+        }
+
+        return $funcionario->getSalario() * 0.85;
+    }
 }
